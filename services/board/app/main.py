338235a254e9dd3b48ed977490e5497435b77f1a
Prashant-SL <prashant.laxmeshwarmath@gmail.com>
2026-04-17 14:44:10 +0530

    Fixed: Method not allowed issue fixed for board services

--- services/board/app/main.py
+++ services/board/app/main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from sqlalchemy import text
 from app.db import SessionLocal, engine, Base
@@ -10,6 +11,18 @@ Base.metadata.create_all(bind=engine)
 app.include_router(board_routes.router)
 app.include_router(column_routes.router)
 app.include_router(task_routes.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://kanbancore-gateway-service.onrender.com"
+    ],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],  # Fixed: changed ["GET, POST"] to ["*"] to allow OPTIONS preflight requests
+    allow_headers=["*"],
+)
 
 @app.get("/healthcheck/board")
 def read_root():
